1e527979ea8715c1747479b4091a2a0a0131dd58
Erik Lew <ek.lew99@gmail.com>
2021-07-06 11:39:46 -0700

    message

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var reqURL = "https://api.boardgameatlas.com/api"
 https://api.boardgameatlas.com/api/search?order_by=popularity&ascending=false&client_id=JLBr5npPhV
 
 function getData(type, filter) {
-    fetch(`https://api.boardgameatlas.com/api/${type}?name=${filter}&client_id=JLBr5npPhV`)
+    fetch(`https://api.boardgameatlas.com/api/${type}?name=${filter}&client_id=YLKK3qmQkg`)
         .then(response => {
             if(!response.ok) {
                 console.log(console.error())
