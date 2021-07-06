ef0a4686d5219863eba86817ad51d7381cb33933
Erik Lew <ek.lew99@gmail.com>
2021-07-06 11:36:58 -0700

    fetch

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var reqURL = "https://api.boardgameatlas.com/api"
+
+https://api.boardgameatlas.com/api/search?order_by=popularity&ascending=false&client_id=JLBr5npPhV
+
+function getData(type, filter) {
+    fetch(`https://api.boardgameatlas.com/api/${type}?name=${filter}&client_id=JLBr5npPhV`)
+        .then(response => {
+            if(!response.ok) {
+                console.log(console.error())
+            } else {
+                return response.json()
+            }
+        })
+}
+getData("search", "Star Trek")
