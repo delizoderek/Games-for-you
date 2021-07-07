a8a36bd2447ac1df72bcb4e0a845cace6d5e5074
TheHebi <nturcotte8@live.com>
2021-07-07 10:12:57 -0700

    mobile formatting in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,70 @@
-console.log("Hello World");
-
-var rawgApi = "a64070dd102e45d4bbd286bc64075645"
-var bgAtlasApi = "id6TuxDAFr"
-var car1 = document.getElementById("car1")
-var car2 = document.getElementById("car2")
-var car3 = document.getElementById("car3")
-var car4 = document.getElementById("car4")
-function carouselImg(){
-    fetch(`https://api.rawg.io/api/games?page_size=20&key=${rawgApi}`)
-    .then(function(data){
-        return data.json();
+var rawgApi = "a64070dd102e45d4bbd286bc64075645";
+var bgAtlasApi = "id6TuxDAFr";
+var car1 = document.getElementById("car1");
+var car2 = document.getElementById("car2");
+var car3 = document.getElementById("car3");
+var car4 = document.getElementById("car4");
+function carouselImg() {
+  fetch(`https://api.rawg.io/api/games?page_size=4&key=${rawgApi}`)
+    .then(function (data) {
+      return data.json();
     })
-    .then(function(data){
-        console.log(data)
-        let index = Math.floor(Math.random() * 20)
-        let index2 = Math.floor(Math.random() * 20)
-        while(index === index2){
-            index2 = Math.floor(Math.random() * 20)
-        }
-        car1.setAttribute("src", `${data.results[index].background_image}`)
-        document.getElementById("carName1").textContent = data.results[index].name
-        car3.setAttribute("src", `${data.results[index2].background_image}`)
-        document.getElementById("carName3").textContent = data.results[index2].name
+    .then(function (data) {
+      let index = Math.floor(Math.random() * 4);
+      let index2 = Math.floor(Math.random() * 4);
+      while (index === index2) {
+        index2 = Math.floor(Math.random() * 20);
+      }
+      car1.setAttribute("src", `${data.results[index].background_image}`);
+      document.getElementById("carName1").textContent =
+        data.results[index].name;
+      car3.setAttribute("src", `${data.results[index2].background_image}`);
+      document.getElementById("carName3").textContent =
+        data.results[index2].name;
+    });
+  fetch(
+    `https://api.boardgameatlas.com/api/search?random=true&client_id=${bgAtlasApi}`
+  )
+    .then(function (data) {
+      return data.json();
     })
-    fetch(`https://api.boardgameatlas.com/api/search?random=true&client_id=${bgAtlasApi}`)
-    .then(function(data){
-        return data.json()
+    .then(function (data) {
+      car2.setAttribute("src", `${data.games[0].image_url}`);
+      document.getElementById("carName2").textContent = data.games[0].name;
+    });
+  fetch(
+    `https://api.boardgameatlas.com/api/search?random=true&client_id=${bgAtlasApi}`
+  )
+    .then(function (data) {
+      return data.json();
     })
-    .then(function(data){
-        console.log(data)
-        car2.setAttribute("src", `${data.games[0].image_url}`)
-        document.getElementById("carName2").textContent = data.games[0].name
-    })
-    fetch(`https://api.boardgameatlas.com/api/search?random=true&client_id=${bgAtlasApi}`)
-    .then(function(data){
-        return data.json()
-
-    })
-    .then(function(data){
-        console.log(data)
-        document.getElementById("carName4").textContent = data.games[0].name
-        car4.setAttribute("src", `${data.games[0].image_url}`)
-    })
+    .then(function (data) {
+      document.getElementById("carName4").textContent = data.games[0].name;
+      car4.setAttribute("src", `${data.games[0].image_url}`);
+    });
 }
-var changeTab = function (event){
-    document.getElementById("nameTab").classList.remove("active")
-    document.getElementById("ageTab").classList.remove("active")
-    document.getElementById("genreTab").classList.remove("active")
-    console.log(event.target)
-    event.target.classList.add("active")
+var changeTab = function (event) {
+  document.getElementById("nameTab").classList.remove("active");
+  document.getElementById("ageTab").classList.remove("active");
+  document.getElementById("genreTab").classList.remove("active");
 
-    document.getElementById("nameSearch").classList.add("hidden")
-    document.getElementById("ageSearch").classList.add("hidden")
-    document.getElementById("genreSearch").classList.add("hidden")
-    var name = event.target.dataset.name
-    console.log(name)
-    if(name === "Name"){
-        document.getElementById("nameSearch").classList.remove("hidden")
-    }else if(name === "Genre"){
-        document.getElementById("genreSearch").classList.remove("hidden")
-    }else if(name === "Age"){
-        document.getElementById("ageSearch").classList.remove("hidden")
-    }
-}
+  event.target.classList.add("active");
 
-document.querySelector("#nameTab").addEventListener("click", changeTab)
-document.querySelector("#genreTab").addEventListener("click", changeTab)
-document.querySelector("#ageTab").addEventListener("click", changeTab)
-carouselImg()
+  document.getElementById("nameSearch").classList.add("hidden");
+  document.getElementById("ageSearch").classList.add("hidden");
+  document.getElementById("genreSearch").classList.add("hidden");
+  var name = event.target.dataset.name;
+
+  if (name === "Name") {
+    document.getElementById("nameSearch").classList.remove("hidden");
+  } else if (name === "Genre") {
+    document.getElementById("genreSearch").classList.remove("hidden");
+  } else if (name === "Age") {
+    document.getElementById("ageSearch").classList.remove("hidden");
+  }
+};
+
+document.querySelector("#nameTab").addEventListener("click", changeTab);
+document.querySelector("#genreTab").addEventListener("click", changeTab);
+document.querySelector("#ageTab").addEventListener("click", changeTab);
+carouselImg();
